d7cf72dc4a1647ce1bc818b89a07e5f6b8b7dd44
Kevin Ingersoll <kingersoll@gmail.com>
2025-04-16 11:56:51 +0100

    set up playground

---
+++ apps/dustkit-playground/index.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { WagmiProvider } from "wagmi";
+import { App } from "./src/App";
+import { wagmiConfig } from "./wagmiConfig";
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.querySelector("#react-root")!);
+root.render(
+  <StrictMode>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </StrictMode>,
+);
