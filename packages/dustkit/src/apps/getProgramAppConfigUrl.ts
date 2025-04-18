203d953bfc22d5956b2388265f95b6102f278dc0
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-04-17 20:06:23 -0600

    pr review

--- packages/dustkit/src/apps/getProgramAppConfigUrl.ts
+++ packages/dustkit/src/apps/getProgramAppConfigUrl.ts
@@ -1,5 +1,7 @@
+import { findCause } from "@latticexyz/common";
 import { getRecord } from "@latticexyz/store/internal";
 import baseWorldConfig from "@latticexyz/world/mud.config";
+import { unknown } from "arktype/internal/keywords/ts.ts";
 import { type Address, type Client, zeroHash } from "viem";
 import { readContract } from "viem/actions";
 import appAbi from "../../out/IAppConfigURI.sol/IAppConfigURI.abi";
@@ -18,7 +20,6 @@ export async function getProgramAppConfigUrl({
   program: ProgramId;
   entity?: EntityId;
 }): Promise<string | undefined> {
-  let url: string | undefined;
   const { system } = await getRecord(client, {
     address: worldAddress,
     table: baseWorldConfig.tables.world__Systems,
@@ -30,19 +31,24 @@ export async function getProgramAppConfigUrl({
   try {
     // We are calling the program system directly here, as programs are required to be private
     // systems so that their hook functions can only be called by the world
-    url = await readContract(client, {
+    const url = await readContract(client, {
       address: system,
       abi: appAbi,
       functionName: "appConfigURI",
       args: [entity ?? zeroHash],
     });
-  } catch (error: any) {
-    if (error?.name === "ContractFunctionExecutionError") {
+    if (url !== "") {
+      return url;
+    }
+  } catch (error: unknown) {
+    if (
+      findCause(
+        error as Error,
+        ({ name }) => name === "ContractFunctionExecutionError",
+      )
+    ) {
       return undefined;
     }
     throw error;
-  }
-  if (url !== "") {
-    return url;
   }
 }
