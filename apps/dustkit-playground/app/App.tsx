495397e45608478fad750c24d2ac65d7279de859
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-05-14 15:52:08 -0600

    fix

--- apps/dustkit-playground/app/App.tsx
+++ apps/dustkit-playground/app/App.tsx
@@ -2,21 +2,44 @@ import defaultProgramAbi from "@dust/programs/out/DefaultProgram.sol/DefaultProg
 import { resourceToHex } from "@latticexyz/common";
 import {
   type AppRpcSchema,
+  type ClientRpcSchema,
+  type MessagePortTransport,
   createMessagePortRpcServer,
+  getMessagePortRpcClient,
+  messagePort,
 } from "dustkit/internal";
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { zeroHash } from "viem";
-import { dustClient } from "./dust";
 
 export function App() {
+  const dustClientRef = useRef<MessagePortTransport<ClientRpcSchema> | null>(
+    null,
+  );
+
+  const loadDustClient = useCallback(async () => {
+    if (!window.opener && !window.parent) {
+      console.error("no parent or opener");
+      return;
+    }
+
+    const rpcClient = await getMessagePortRpcClient(
+      window.opener ?? window.parent,
+    );
+
+    const newDustClient = messagePort<ClientRpcSchema>(rpcClient);
+    dustClientRef.current = newDustClient;
+  }, []);
+
   useEffect(() => {
-    createMessagePortRpcServer<AppRpcSchema>({
+    loadDustClient();
+
+    return createMessagePortRpcServer<AppRpcSchema>({
       async dustApp_init(params) {
         console.info("client asked this app to initialize with", params);
         return { success: true };
       },
     });
-  }, []);
+  }, [loadDustClient]);
 
   return (
     <div>
@@ -25,7 +48,12 @@ export function App() {
         <button
           type="button"
           onClick={async () => {
-            await dustClient({}).request({
+            if (!dustClientRef.current) {
+              console.error("no dust client");
+              return;
+            }
+
+            await dustClientRef.current({}).request({
               method: "dustClient_setWaypoint",
               params: {
                 entity: "0x",
@@ -39,7 +67,12 @@ export function App() {
         <button
           type="button"
           onClick={async () => {
-            await dustClient({}).request({
+            if (!dustClientRef.current) {
+              console.error("no dust client");
+              return;
+            }
+
+            await dustClientRef.current({}).request({
               method: "dustClient_systemCall",
               params: [
                 {
