e9217d57a1c05dc11e3472f16afa475449e19830
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-05-16 09:51:38 -0600

    add pos hud app (#271)
    * wip
    
    * fix
    
    * get position
    
    * rename
    
    * fix
    
    * remove
    
    * fix
    
    * add todo
    
    * fix
    
    * replace ts with forge script
    
    * simplify
    
    * use mprocs
    
    * fix
    
    * fix

---
+++ apps/pos-hud/App.tsx
@@ -0,0 +1,80 @@
+import {
+  type AppRpcSchema,
+  type ClientRpcSchema,
+  type MessagePortTransport,
+  createMessagePortRpcServer,
+  getMessagePortRpcClient,
+  messagePort,
+} from "dustkit/internal";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export function App() {
+  const dustClientRef = useRef<MessagePortTransport<ClientRpcSchema> | null>(
+    null,
+  );
+  const [playerPosition, setPlayerPosition] = useState<{
+    x: number;
+    y: number;
+    z: number;
+  } | null>(null);
+
+  const updatePlayerPosition = useCallback(async () => {
+    if (!dustClientRef.current) {
+      console.error("no dust client");
+      return;
+    }
+
+    const position = await dustClientRef.current({}).request({
+      method: "dustClient_getPlayerPosition",
+      params: {
+        entity: "0x",
+      },
+    });
+
+    setPlayerPosition(position);
+  }, []);
+
+  const loadDustClient = useCallback(async () => {
+    if (!window.opener && !window.parent) {
+      console.error("no parent or opener");
+      return;
+    }
+
+    const rpcClient = await getMessagePortRpcClient(
+      window.opener ?? window.parent,
+    );
+
+    const newDustClient = messagePort<ClientRpcSchema>(rpcClient);
+    dustClientRef.current = newDustClient;
+  }, []);
+
+  useEffect(() => {
+    loadDustClient();
+
+    return createMessagePortRpcServer<AppRpcSchema>({
+      async dustApp_init(params) {
+        console.info("client asked this app to initialize with", params);
+        return { success: true };
+      },
+    });
+  }, [loadDustClient]);
+
+  useEffect(() => {
+    updatePlayerPosition();
+
+    // run every 100ms
+    const interval = setInterval(updatePlayerPosition, 100);
+
+    return () => clearInterval(interval);
+  }, [updatePlayerPosition]);
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "row", gap: 5, color: "white" }}
+    >
+      <span>x: {Math.floor(playerPosition?.x ?? 0)}</span>
+      <span>y: {Math.floor(playerPosition?.y ?? 0)}</span>
+      <span>z: {Math.floor(playerPosition?.z ?? 0)}</span>
+    </div>
+  );
+}
---
+++ apps/pos-hud/RegisterApp.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { metadataSystem } from
+  "@latticexyz/world-module-metadata/src/codegen/experimental/systems/MetadataSystemLib.sol";
+import { ResourceId, WorldResourceIdInstance, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+
+import { ResourceIds } from "@latticexyz/store/src/codegen/tables/ResourceIds.sol";
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+
+import { IWorld } from "@dust/world/src/codegen/world/IWorld.sol";
+
+contract RegisterApp is Script {
+  function run(address worldAddress) external {
+    // Specify a store so that you can use tables directly in PostDeploy
+    StoreSwitch.setStoreAddress(worldAddress);
+
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    // Start broadcasting transactions from the deployer account
+    vm.startBroadcast(deployerPrivateKey);
+
+    IWorld world = IWorld(worldAddress);
+
+    ResourceId appNamespaceId = WorldResourceIdLib.encodeNamespace("pos-hud");
+    if (!ResourceIds.getExists(appNamespaceId)) {
+      world.registerNamespace(appNamespaceId);
+    }
+
+    metadataSystem.setResourceTag(appNamespaceId, "dust.appConfigUrl", bytes("http://localhost:5501/dust-app.json"));
+
+    vm.stopBroadcast();
+  }
+}
