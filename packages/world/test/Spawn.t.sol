1a9ac8e7ee934170b61ab8aae2042db2b71b2241
vdrg <vdrg@protonmail.ch>
2025-05-19 16:01:38 -0400

    Add gas report to spawn tile

--- packages/world/test/Spawn.t.sol
+++ packages/world/test/Spawn.t.sol
@@ -80,7 +80,7 @@ contract SpawnTest is DustTest {
     world.randomSpawn(pastBlock, y);
   }
 
-  function testSpawn() public {
+  function testSpawnTile() public {
     address alice = vm.randomAddress();
     Vec3 spawnCoord = vec3(0, 1, 0);
     Vec3 spawnTileCoord = spawnCoord - vec3(0, 1, 0);
@@ -115,7 +115,9 @@ contract SpawnTest is DustTest {
 
     // Spawn alice
     vm.prank(alice);
+    startGasReport("spawn with spawn tile");
     EntityId playerEntityId = world.spawn(spawnTileEntityId, spawnCoord, 1, "");
+    endGasReport();
     assertTrue(TestEntityUtils.exists(playerEntityId));
   }
 
