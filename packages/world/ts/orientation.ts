44f93054effc3a8c1c394b91638e5a4d1e9091c2
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-05-21 11:56:47 -0600

    fix ts doesnt know size

--- packages/world/ts/orientation.ts
+++ packages/world/ts/orientation.ts
@@ -26,17 +26,33 @@ export type Permute = readonly [Axis, Axis, Axis];
 export type Reflect = readonly [0 | 1, 0 | 1, 0 | 1];
 
 // 6 possible permutations
-export const PERMUTATIONS: Permute[] = [
+export const PERMUTATIONS: readonly [
+  Permute,
+  Permute,
+  Permute,
+  Permute,
+  Permute,
+  Permute,
+] = [
   [0, 1, 2], // Original orientation
   [0, 2, 1], // Swap y and z
   [1, 0, 2], // Swap x and y
   [1, 2, 0], // Rotate x->y->z->x
   [2, 0, 1], // Rotate x->z->y->x
   [2, 1, 0], // Swap x and z
-];
+] as const;
 
 // 8 possible reflections
-export const REFLECTIONS: Reflect[] = [
+export const REFLECTIONS: readonly [
+  Reflect,
+  Reflect,
+  Reflect,
+  Reflect,
+  Reflect,
+  Reflect,
+  Reflect,
+  Reflect,
+] = [
   [0, 0, 0], // No reflection
   [1, 0, 0], // Reflect x
   [0, 1, 0], // Reflect y
@@ -45,7 +61,7 @@ export const REFLECTIONS: Reflect[] = [
   [1, 0, 1], // Reflect x and z
   [0, 1, 1], // Reflect y and z
   [1, 1, 1], // Reflect all axes
-];
+] as const;
 
 const CANONICAL_UP: Vec3 = [0, 1, 0];
 const CANONICAL_FORWARD: Vec3 = [1, 0, 0];
