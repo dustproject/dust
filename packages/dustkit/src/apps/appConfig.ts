3f70ee31c6956927fac46731fa5cf960d66f7ba0
Kevin Ingersoll <kingersoll@gmail.com>
2025-05-22 19:41:21 +0900

    apps can be pinnable (#284)

--- packages/dustkit/src/apps/appConfig.ts
+++ packages/dustkit/src/apps/appConfig.ts
@@ -3,19 +3,21 @@ import { type } from "arktype";
 export const appConfig = type({
   name: "string",
   startUrl: "string",
-  "frame?": {
+  "defaultSize?": {
     width: "number",
     height: "number",
   },
+  "isPinnable?": "boolean",
 });
 export type AppConfig = typeof appConfig.infer;
 
 export const appConfigInput = type({
   name: "string",
   "startUrl?": "string",
-  "frame?": {
+  "defaultSize?": {
     width: "number",
     height: "number",
   },
+  "isPinnable?": "boolean",
 });
 export type AppConfigInput = typeof appConfigInput.infer;
