1986435e231e2e3a995cb5822fdd01190da33c7c
Kevin Ingersoll <kingersoll@gmail.com>
2025-06-06 10:05:45 +0100

    fix type validation

--- packages/dustkit/src/apps/appConfig.ts
+++ packages/dustkit/src/apps/appConfig.ts
@@ -3,19 +3,27 @@ import { type } from "arktype";
 export const appConfig = type({
   name: "string",
   startUrl: "string",
-  "defaultSize?": {
-    width: "number",
-    height: "number",
-  },
+  "defaultSize?": [
+    "undefined",
+    "|",
+    {
+      width: "number",
+      height: "number",
+    },
+  ],
 });
 export type AppConfig = typeof appConfig.infer;
 
 export const appConfigInput = type({
   name: "string",
-  "startUrl?": "string",
-  "defaultSize?": {
-    width: "number",
-    height: "number",
-  },
+  "startUrl?": "undefined | string",
+  "defaultSize?": [
+    "undefined",
+    "|",
+    {
+      width: "number",
+      height: "number",
+    },
+  ],
 });
 export type AppConfigInput = typeof appConfigInput.infer;
