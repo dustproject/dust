f97a5bd821298492bf08c7fda2dbf01ea04f7a0b
Kevin Ingersoll <kingersoll@gmail.com>
2025-06-06 10:11:06 +0100

    no undefined in config for json schema compat

--- packages/dustkit/src/apps/appConfig.ts
+++ packages/dustkit/src/apps/appConfig.ts
@@ -3,27 +3,19 @@ import { type } from "arktype";
 export const appConfig = type({
   name: "string",
   startUrl: "string",
-  "defaultSize?": [
-    "undefined",
-    "|",
-    {
-      width: "number",
-      height: "number",
-    },
-  ],
+  "defaultSize?": {
+    width: "number",
+    height: "number",
+  },
 });
 export type AppConfig = typeof appConfig.infer;
 
 export const appConfigInput = type({
   name: "string",
-  "startUrl?": "undefined | string",
-  "defaultSize?": [
-    "undefined",
-    "|",
-    {
-      width: "number",
-      height: "number",
-    },
-  ],
+  "startUrl?": "string",
+  "defaultSize?": {
+    width: "number",
+    height: "number",
+  },
 });
 export type AppConfigInput = typeof appConfigInput.infer;
