47d6fce2c0649f96319a744c16db7e74fc192ed3
V <vdrg@lattice.xyz>
2025-06-16 13:08:42 -0400

    fix: seed tracking (#342)

--- packages/world/CHANGELOG.md
+++ packages/world/CHANGELOG.md
@@ -3,3 +3,4 @@
 ## 2024-06-16
 
 - Wake up player when mining their bed instead of killing them.
+- Fix: Seeds and saplings in circulation were not being tracked correctly.
--- packages/world/test/Mine.t.sol
+++ packages/world/test/Mine.t.sol
@@ -251,9 +251,11 @@ contract MineTest is DustTest {
     vm.prank(alice);
     world.mineUntilDestroyed(aliceEntityId, farmlandCoord + vec3(0, 1, 0), "");
 
+    assertEq(ResourceCount.get(ObjectTypes.WheatSeed), 1, "Wheat seeds were not removed from circulation");
+
     // Verify drops
     assertInventoryHasObject(aliceEntityId, ObjectTypes.Wheat, 1);
-    assertInventoryHasObject(aliceEntityId, ObjectTypes.WheatSeed, 0);
+    assertInventoryHasObject(aliceEntityId, ObjectTypes.WheatSeed, 1);
 
     // Verify crop no longer exists
     assertEq(EntityObjectType.get(cropEntityId), ObjectTypes.Air, "Crop wasn't removed after harvesting");
@@ -303,9 +305,11 @@ contract MineTest is DustTest {
     vm.prank(alice);
     world.mineUntilDestroyed(aliceEntityId, farmlandCoord, "");
 
+    assertEq(ResourceCount.get(ObjectTypes.WheatSeed), 1, "Seed drop was not removed from circulation");
+
     // Verify drops
     assertInventoryHasObject(aliceEntityId, ObjectTypes.Wheat, 1);
-    assertInventoryHasObject(aliceEntityId, ObjectTypes.WheatSeed, 0);
+    assertInventoryHasObject(aliceEntityId, ObjectTypes.WheatSeed, 1);
 
     // Verify crop no longer exists
     assertEq(EntityObjectType.get(cropEntityId), ObjectTypes.Air, "Crop wasn't removed after harvesting");
