d5a761ede2f8af35722b0aa2d67dea83a66574d9
V <vdrg@lattice.xyz>
2025-06-23 15:01:53 -0400

    Add transferAmounts function (#356)
    Signed-off-by: V <vdrg@lattice.xyz>

--- packages/world/CHANGELOG.md
+++ packages/world/CHANGELOG.md
@@ -2,8 +2,9 @@
 
 ## 2024-06-23
 
-- Lazily assign mass on mine for blocks that were set up incorrectly.
 - Pack move directions in a single uint256 to decrease tx calldata size.
+- New transferAmounts function that doesn't require specifying destination slots, which simplifies app interactions with chests.
+- Fix: Lazily assign mass on mine for blocks that were set up incorrectly.
 - Fix: Don't revert if trying to pickup multiple drops that don't fit in the inventory.
 
 ## 2024-06-16
--- packages/world/src/systems/TransferSystem.sol
+++ packages/world/src/systems/TransferSystem.sol
@@ -5,7 +5,7 @@ import { System } from "@latticexyz/world/src/System.sol";
 
 import { EnergyData } from "../codegen/tables/Energy.sol";
 
-import { InventoryUtils, SlotData, SlotTransfer } from "../utils/InventoryUtils.sol";
+import { InventoryUtils, SlotAmount, SlotData, SlotTransfer } from "../utils/InventoryUtils.sol";
 import { TransferNotification, notify } from "../utils/NotifUtils.sol";
 
 import { EntityId } from "../types/EntityId.sol";
@@ -40,27 +40,71 @@ contract TransferSystem is System {
       // If transferring between different inventories,
       // caller must be involved and the hook should be called for other party
       require(callerIsFrom || callerIsTo, "Caller is not involved in transfer");
-      target = (callerIsFrom ? to : from);
-    }
-
-    if (target._exists()) {
+      target = callerIsFrom ? to : from;
       caller.requireConnected(target);
-      ObjectType targetType = target._getObjectType();
-      require(targetType != ObjectTypes.Player, "Cannot access another player's inventory");
-      require(!targetType.isPassThrough(), "Cannot transfer directly to pass-through object");
     }
 
     SlotData[] memory deposits;
     SlotData[] memory withdrawals;
-    {
-      (SlotData[] memory fromSlotData, SlotData[] memory toSlotData) = InventoryUtils.transfer(from, to, transfers);
 
-      // Get deposits and withdrawals FROM THE TARGET's PERSPECTIVE
-      // If target == to, we are depositing fromSlotData and withdrawing toSlotData
-      (deposits, withdrawals) = target == to ? (fromSlotData, toSlotData) : (toSlotData, fromSlotData);
-    }
+    (SlotData[] memory fromSlotData, SlotData[] memory toSlotData) = InventoryUtils.transfer(from, to, transfers);
 
+    // Get deposits and withdrawals FROM THE TARGET's PERSPECTIVE
+    // If target == to, we are depositing fromSlotData and withdrawing toSlotData
+    (deposits, withdrawals) = target == to ? (fromSlotData, toSlotData) : (toSlotData, fromSlotData);
+
+    _validateTargetAndNotify(caller, target, deposits, withdrawals, extraData);
+  }
+
+  function transferAmounts(
+    EntityId caller,
+    EntityId from,
+    EntityId to,
+    SlotAmount[] calldata amounts,
+    bytes calldata extraData
+  ) external {
+    caller.activate();
+
+    // SlotAmount transfers don't support self-transfers
+    require(from != to, "Cannot transfer amounts to self");
+
+    bool callerIsFrom = (caller == from);
+    bool callerIsTo = (caller == to);
+
+    // Caller must be involved in the transfer
+    require(callerIsFrom || callerIsTo, "Caller is not involved in transfer");
+
+    // Determine which entity needs hook validation
+    EntityId target = callerIsFrom ? to : from;
+
+    caller.requireConnected(target);
+
+    // Execute the transfer and get the slot data
+    SlotData[] memory fromSlotData = InventoryUtils.transfer(from, to, amounts);
+
+    // Get deposits and withdrawals FROM THE TARGET's PERSPECTIVE
+    // If target == to, we are depositing fromSlotData
+    // If target == from, we are withdrawing fromSlotData
+    SlotData[] memory deposits;
+    SlotData[] memory withdrawals;
+
+    (deposits, withdrawals) = target == to ? (fromSlotData, new SlotData[](0)) : (new SlotData[](0), fromSlotData);
+
+    _validateTargetAndNotify(caller, target, deposits, withdrawals, extraData);
+  }
+
+  function _validateTargetAndNotify(
+    EntityId caller,
+    EntityId target,
+    SlotData[] memory deposits,
+    SlotData[] memory withdrawals,
+    bytes calldata extraData
+  ) internal {
     if (target._exists()) {
+      ObjectType targetType = target._getObjectType();
+      require(targetType != ObjectTypes.Player, "Cannot access another player's inventory");
+      require(!targetType.isPassThrough(), "Cannot transfer directly to pass-through object");
+
       Hooks.TransferContext memory ctx = Hooks.TransferContext({
         caller: caller,
         target: target,
