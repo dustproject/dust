048ba94b03bb51469421d016f1d39ff8d425303d
vdrg <vdrg@protonmail.ch>
2025-07-08 16:24:25 -0400

    Add overloaded functions that use msgSender instead of caller

--- packages/programs/src/DefaultProgramSystem.sol
+++ packages/programs/src/DefaultProgramSystem.sol
@@ -22,13 +22,6 @@ contract DefaultProgramSystem is System {
     return createAccessGroup(owner);
   }
 
-  function setAccessGroup(EntityId caller, EntityId target, uint256 groupId) public {
-    caller.validateCaller();
-    (uint256 currentGroupId,) = getGroupId(target);
-    _requireOwner(currentGroupId, caller);
-    EntityAccessGroup.set(target, groupId);
-  }
-
   /// @dev This function can be called by other entities to get a new access group assigned to themselves
   function setAccessGroup(EntityId caller, address groupOwner) external {
     caller.validateCaller();
@@ -39,10 +32,18 @@ contract DefaultProgramSystem is System {
     EntityAccessGroup.set(caller, groupId);
   }
 
+  /* Explicit caller (EntityId) */
+
+  function setAccessGroup(EntityId caller, EntityId target, uint256 groupId) public {
+    caller.validateCaller();
+    (uint256 currentGroupId,) = getGroupId(target);
+    _requireOwner(currentGroupId, caller);
+    EntityAccessGroup.set(target, groupId);
+  }
+
   function setMembership(EntityId caller, uint256 groupId, EntityId member, bool allowed) public {
     caller.validateCaller();
-    _requireOwner(groupId, caller);
-    AccessGroupMember.set(groupId, member, allowed);
+    _setMembership(caller, groupId, member, allowed);
   }
 
   function setMembership(EntityId caller, uint256 groupId, address member, bool allowed) external {
@@ -59,20 +60,49 @@ contract DefaultProgramSystem is System {
     setMembership(caller, target, EntityTypeLib.encodePlayer(member), allowed);
   }
 
-  function setOwner(EntityId caller, uint256 groupId, EntityId newOwner) external {
+  function setOwner(EntityId caller, uint256 groupId, EntityId newOwner) public {
     caller.validateCaller();
     _requireOwner(groupId, caller);
     AccessGroupOwner.set(groupId, newOwner);
   }
 
-  function setTextSignContent(EntityId caller, EntityId target, string memory content) external {
+  function setTextSignContent(EntityId caller, EntityId target, string memory content) public {
     caller.validateCaller();
     _requireMember(caller, target);
     TextSignContent.set(target, content);
   }
 
-  function getEntityGroupId(EntityId target) external view returns (uint256 groupId, bool defaultDeny) {
-    return getGroupId(target);
+  /* Implicit caller (_msgSender()) */
+
+  function setAccessGroup(EntityId target, uint256 groupId) public {
+    setAccessGroup(EntityTypeLib.encodePlayer(_msgSender()), target, groupId);
+  }
+
+  function setMembership(uint256 groupId, EntityId member, bool allowed) public {
+    _setMembership(EntityTypeLib.encodePlayer(_msgSender()), groupId, member, allowed);
+  }
+
+  function setMembership(uint256 groupId, address member, bool allowed) external {
+    setMembership(groupId, EntityTypeLib.encodePlayer(member), allowed);
+  }
+
+  function setMembership(EntityId target, EntityId member, bool allowed) public {
+    (uint256 groupId,) = getGroupId(target);
+    require(groupId != 0, "Target entity has no access group");
+    setMembership(groupId, member, allowed);
+  }
+
+  function setMembership(EntityId target, address member, bool allowed) external {
+    setMembership(target, EntityTypeLib.encodePlayer(member), allowed);
+  }
+
+  function setOwner(uint256 groupId, EntityId newOwner) external {
+    setOwner(EntityTypeLib.encodePlayer(_msgSender()), groupId, newOwner);
+  }
+
+  function setTextSignContent(EntityId target, string memory content) external {
+    EntityId caller = EntityTypeLib.encodePlayer(_msgSender());
+    setTextSignContent(caller, target, content);
   }
 
   function _requireMember(EntityId caller, EntityId target) private view {
@@ -81,5 +111,10 @@ contract DefaultProgramSystem is System {
 
   function _requireOwner(uint256 groupId, EntityId caller) private view {
     require(AccessGroupOwner.get(groupId) == caller, "Only the owner of the access group can call this function");
+  }
+
+  function _setMembership(EntityId caller, uint256 groupId, EntityId member, bool allowed) private {
+    _requireOwner(groupId, caller);
+    AccessGroupMember.set(groupId, member, allowed);
   }
 }
