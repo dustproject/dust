bee3565754ca025508f2842ee19db67d11312a31
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-07-23 18:00:45 -0600

    simplify

--- packages/world/ts/chunk.ts
+++ packages/world/ts/chunk.ts
@@ -2,11 +2,11 @@ import type { ReadonlyVec3, Vec3 } from "./vec3";
 
 export const chunkSize = 16;
 
-export function voxelToChunkPos(vec3: ReadonlyVec3): Vec3 {
+export function voxelToChunkPos([x, y, z]: ReadonlyVec3): Vec3 {
   return [
-    Math.floor(vec3[0] / chunkSize),
-    Math.floor(vec3[1] / chunkSize),
-    Math.floor(vec3[2] / chunkSize),
+    Math.floor(x / chunkSize),
+    Math.floor(y / chunkSize),
+    Math.floor(z / chunkSize),
   ];
 }
 
