b44342fd729c53a244c8f1a557805aff3e4d51fe
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-08-19 22:45:34 -0600

    change to non root in _setPlayerName

--- packages/world/script/migrations/3-init-player-names/migration.s.sol
+++ packages/world/script/migrations/3-init-player-names/migration.s.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.24;
 
-import { PlayerName } from "../../../src/codegen/tables/PlayerName.sol";
-import { ReversePlayerName } from "../../../src/codegen/tables/ReversePlayerName.sol";
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 import { console } from "forge-std/console.sol";
-import { LibString } from "solady/utils/LibString.sol";
+
+import { _setPlayerName } from "../../../src/systems/NameSystem.sol";
 
 import { DustScript } from "../../DustScript.sol";
 
@@ -46,24 +45,4 @@ contract InitPlayerNames is DustScript {
 
     console.log("\nMigration Complete!");
   }
-}
-
-function _setPlayerName(address player, string memory name) {
-  uint128 validCharacters =
-    LibString.to7BitASCIIAllowedLookup("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_");
-
-  require(bytes(name).length > 0 && bytes(name).length <= 32, "Name must be 1-32 characters long.");
-  require(LibString.is7BitASCII(name, validCharacters), "Name has invalid characters.");
-
-  bytes32 storedName = LibString.toSmallString(name);
-  require(ReversePlayerName.get(storedName) == address(0), "Name is already in use.");
-
-  // free up previous name
-  bytes32 previousName = PlayerName.get(player);
-  if (previousName != bytes32(0)) {
-    ReversePlayerName.deleteRecord(previousName);
-  }
-
-  ReversePlayerName.set({ player: player, name: storedName });
-  PlayerName.set({ player: player, name: storedName });
 }
