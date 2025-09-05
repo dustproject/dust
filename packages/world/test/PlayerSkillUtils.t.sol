9be65bcd3d8111b3f078bfbd69d3370ff1a6304b
vdrg <vdrg@protonmail.ch>
2025-09-05 19:31:12 -0400

    Add skill fuzz tests

--- packages/world/test/PlayerSkillUtils.t.sol
+++ packages/world/test/PlayerSkillUtils.t.sol
@@ -2,23 +2,21 @@
 pragma solidity >=0.8.24;
 
 import {
-  SKILL_ENERGY_MAX_DISCOUNT_WAD, SKILL_MINING_MASS_TO_MAX, SKILL_MOVE_ENERGY_TO_MAX, WAD
+  SKILL_CRAFT_MASS_ENERGY_TO_MAX,
+  SKILL_ENERGY_MAX_DISCOUNT_WAD,
+  SKILL_MINING_MASS_TO_MAX,
+  SKILL_MOVE_ENERGY_TO_MAX
 } from "../src/Constants.sol";
 import { ActivityType } from "../src/codegen/common.sol";
-
 import { Death } from "../src/codegen/tables/Death.sol";
 import { PlayerProgress, PlayerProgressData } from "../src/codegen/tables/PlayerProgress.sol";
-
 import { EntityId } from "../src/types/EntityId.sol";
-import { ObjectTypes } from "../src/types/ObjectType.sol";
-
+import { ObjectType, ObjectTypes } from "../src/types/ObjectType.sol";
 import { DustTest } from "./DustTest.sol";
 import { TestPlayerSkillUtils } from "./utils/TestUtils.sol";
 
 contract PlayerSkillUtilsTest is DustTest {
   function _setProgress(EntityId player, ActivityType activity, uint128 progress) internal {
-    // Set current exactly to progress; keep accumulated at 0 so floor doesn't dominate.
-    // Align deaths to zero and lastUpdatedAt to now to avoid decay.
     PlayerProgress.set(
       player,
       activity,
@@ -30,20 +28,17 @@ contract PlayerSkillUtilsTest is DustTest {
   function testMoveMultiplierZeroAndCap() public {
     (, EntityId aliceEntityId,) = setupFlatChunkWithPlayer();
 
-    // Zero progress → no discount (1.0x)
     _setProgress(aliceEntityId, ActivityType.MoveEnergy, 0);
     uint256 m0 = TestPlayerSkillUtils.getMoveEnergyMultiplierWad(aliceEntityId);
-    assertEq(m0, WAD, "zero progress should yield 1.0x multiplier");
+    assertEq(m0, 1e18, "zero progress should yield 1.0x multiplier");
 
-    // At or above cap → max discount
     _setProgress(aliceEntityId, ActivityType.MoveEnergy, SKILL_MOVE_ENERGY_TO_MAX);
     uint256 mCap = TestPlayerSkillUtils.getMoveEnergyMultiplierWad(aliceEntityId);
-    assertEq(mCap, WAD - SKILL_ENERGY_MAX_DISCOUNT_WAD, "cap progress should yield max discount");
+    assertEq(mCap, 1e18 - SKILL_ENERGY_MAX_DISCOUNT_WAD, "cap progress should yield max discount");
 
-    // Above cap still saturates at max discount
     _setProgress(aliceEntityId, ActivityType.MoveEnergy, SKILL_MOVE_ENERGY_TO_MAX + 1);
     uint256 mOver = TestPlayerSkillUtils.getMoveEnergyMultiplierWad(aliceEntityId);
-    assertEq(mOver, WAD - SKILL_ENERGY_MAX_DISCOUNT_WAD, "over-cap should saturate at max discount");
+    assertEq(mOver, 1e18 - SKILL_ENERGY_MAX_DISCOUNT_WAD, "over-cap should saturate at max discount");
   }
 
   function testMonotonicity() public {
@@ -55,32 +50,91 @@ contract PlayerSkillUtilsTest is DustTest {
     _setProgress(aliceEntityId, ActivityType.MoveEnergy, SKILL_MOVE_ENERGY_TO_MAX / 2);
     uint256 m2 = TestPlayerSkillUtils.getMoveEnergyMultiplierWad(aliceEntityId);
 
-    // More progress => multiplier should be <= (never larger)
     assertLe(m2, m1, "multiplier must be monotone non-increasing with progress");
-  }
-
-  function testTillHasNoDiscount() public {
-    (, EntityId aliceEntityId,) = setupFlatChunkWithPlayer();
-    uint256 m = TestPlayerSkillUtils.getTillEnergyMultiplierWad(aliceEntityId);
-    assertEq(m, WAD, "tilling should not have a discount");
   }
 
   function testMinePickMultiplierAtCap() public {
     (, EntityId aliceEntityId,) = setupFlatChunkWithPlayer();
 
-    // For mining with pick, activity is MinePickMass
     _setProgress(aliceEntityId, ActivityType.MinePickMass, SKILL_MINING_MASS_TO_MAX);
-
     uint256 m = TestPlayerSkillUtils.getMineEnergyMultiplierWad(aliceEntityId, ObjectTypes.WoodenPick);
-
-    assertEq(m, WAD - SKILL_ENERGY_MAX_DISCOUNT_WAD, "mining at cap should yield max discount");
+    assertEq(m, 1e18 - SKILL_ENERGY_MAX_DISCOUNT_WAD, "mining at cap should yield max discount");
   }
 
   function testCraftStationSpecific() public {
     (, EntityId aliceEntityId,) = setupFlatChunkWithPlayer();
-    // Use BuildMass progress to ensure independence; station type is read only to select activity category.
-    _setProgress(aliceEntityId, ActivityType.CraftWorkbenchMass, SKILL_MOVE_ENERGY_TO_MAX);
+    _setProgress(aliceEntityId, ActivityType.CraftWorkbenchMass, SKILL_CRAFT_MASS_ENERGY_TO_MAX);
     uint256 mWorkbench = TestPlayerSkillUtils.getCraftEnergyMultiplierWad(aliceEntityId, ObjectTypes.Workbench);
-    assertEq(mWorkbench, WAD - SKILL_ENERGY_MAX_DISCOUNT_WAD, "workbench progress at cap, max discount");
+    assertEq(mWorkbench, 1e18 - SKILL_ENERGY_MAX_DISCOUNT_WAD, "workbench progress at cap => max discount");
+  }
+
+  // Fuzz tests
+
+  function testMoveMultiplierFuzz(uint128 progress) public {
+    (, EntityId aliceEntityId,) = setupFlatChunkWithPlayer();
+
+    _setProgress(aliceEntityId, ActivityType.MoveEnergy, progress);
+    uint256 m = TestPlayerSkillUtils.getMoveEnergyMultiplierWad(aliceEntityId);
+
+    assertLe(m, 1e18, "multiplier must not exceed 1.0");
+    assertGe(m, 1e18 - SKILL_ENERGY_MAX_DISCOUNT_WAD, "multiplier must not be below max-discount bound");
+
+    if (progress == 0) assertEq(m, 1e18, "zero progress must yield no discount");
+    if (progress >= SKILL_MOVE_ENERGY_TO_MAX) assertEq(m, 1e18 - SKILL_ENERGY_MAX_DISCOUNT_WAD);
+  }
+
+  function testMoveMultiplierMonotonicFuzz(uint128 a, uint128 b) public {
+    (, EntityId aliceEntityId,) = setupFlatChunkWithPlayer();
+
+    uint128 lo = a <= b ? a : b;
+    uint128 hi = a <= b ? b : a;
+
+    _setProgress(aliceEntityId, ActivityType.MoveEnergy, lo);
+    uint256 mLo = TestPlayerSkillUtils.getMoveEnergyMultiplierWad(aliceEntityId);
+
+    _setProgress(aliceEntityId, ActivityType.MoveEnergy, hi);
+    uint256 mHi = TestPlayerSkillUtils.getMoveEnergyMultiplierWad(aliceEntityId);
+
+    assertLe(mHi, mLo, "multiplier must be monotone non-increasing with progress");
+  }
+
+  function testCraftMultiplierFuzz(uint128 progress, uint8 stationIdx) public {
+    ObjectType[6] memory stations = [
+      ObjectTypes.Workbench,
+      ObjectTypes.Powerstone,
+      ObjectTypes.Furnace,
+      ObjectTypes.Stonecutter,
+      ObjectTypes.Anvil,
+      ObjectTypes.Null
+    ];
+
+    stationIdx = uint8(bound(stationIdx, 0, stations.length - 1));
+    ObjectType station = stations[stationIdx];
+
+    (, EntityId aliceEntityId,) = setupFlatChunkWithPlayer();
+
+    ActivityType activity;
+    if (station == ObjectTypes.Workbench) activity = ActivityType.CraftWorkbenchMass;
+    else if (station == ObjectTypes.Powerstone) activity = ActivityType.CraftPowerstoneMass;
+    else if (station == ObjectTypes.Furnace) activity = ActivityType.CraftFurnaceMass;
+    else if (station == ObjectTypes.Stonecutter) activity = ActivityType.CraftStonecutterMass;
+    else if (station == ObjectTypes.Anvil) activity = ActivityType.CraftAnvilMass;
+    else activity = ActivityType.CraftHandMass;
+
+    _setProgress(aliceEntityId, activity, progress);
+
+    uint256 m = TestPlayerSkillUtils.getCraftEnergyMultiplierWad(aliceEntityId, station);
+    assertLe(m, 1e18, "multiplier must not exceed 1.0");
+    assertGe(m, 1e18 - SKILL_ENERGY_MAX_DISCOUNT_WAD, "multiplier below lower bound");
+
+    if (progress == 0) assertEq(m, 1e18, "zero progress must yield no discount");
+    if (progress >= SKILL_CRAFT_MASS_ENERGY_TO_MAX) assertEq(m, 1e18 - SKILL_ENERGY_MAX_DISCOUNT_WAD);
+  }
+
+  function testMineMultiplierNoToolIsOne() public {
+    (, EntityId aliceEntityId,) = setupFlatChunkWithPlayer();
+    _setProgress(aliceEntityId, ActivityType.MinePickMass, SKILL_MINING_MASS_TO_MAX);
+    uint256 m = TestPlayerSkillUtils.getMineEnergyMultiplierWad(aliceEntityId, ObjectTypes.Null);
+    assertEq(m, 1e18, "no applicable mining progress => no discount");
   }
 }
