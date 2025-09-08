6ba3221c4b88caadefce2ce254aa1956d05ecea0
Kevin Ingersoll <kingersoll@gmail.com>
2025-09-08 11:32:14 +0900

    move logs to debug

--- packages/dustkit/src/realtime/createSocket.ts
+++ packages/dustkit/src/realtime/createSocket.ts
@@ -1,3 +1,5 @@
+import { debug } from "./debug";
+
 export type Socket = {
   on<eventType extends keyof WebSocketEventMap>(
     type: eventType,
@@ -20,12 +22,12 @@ export function createSocket({
   } satisfies Record<keyof WebSocketEventMap, Set<(event: any) => void>>;
 
   async function open() {
-    console.debug("open(): calling connect");
+    debug("open(): calling connect");
     const socket = connect();
-    console.debug("open(): binding open listener/promise");
+    debug("open(): binding open listener/promise");
     const isOpen = waitForOpen(socket);
 
-    console.debug("open(): binding listeners");
+    debug("open(): binding listeners");
     function notify(event: Event) {
       for (const listener of listeners[event.type as keyof WebSocketEventMap]) {
         listener(event);
@@ -36,10 +38,10 @@ export function createSocket({
     socket.addEventListener("close", notify);
     socket.addEventListener("error", notify);
 
-    console.debug("open(): waiting for open");
+    debug("open(): waiting for open");
     await isOpen;
 
-    console.debug("open(): returning socket");
+    debug("open(): returning socket");
     return socket;
   }
 
@@ -49,38 +51,36 @@ export function createSocket({
   async function getSocket() {
     if (socketPromise) {
       if (closed) {
-        console.debug("getSocket(): socket was closed, returning socket");
+        debug("getSocket(): socket was closed, returning socket");
         return socketPromise;
       }
-      console.debug("getSocket(): have socket");
+      debug("getSocket(): have socket");
 
       const currentSocket = socketPromise;
-      console.debug("getSocket(): awaiting socket");
+      debug("getSocket(): awaiting socket");
       const socket = await currentSocket;
       if (currentSocket !== socketPromise) {
-        console.debug(
+        debug(
           "getSocket(): socket promise changed while awaiting, trying again...",
         );
         return getSocket();
       }
       if (socket.readyState !== socket.OPEN) {
-        console.debug(
-          "getSocket(): socket not open, unsetting and trying again...",
-        );
+        debug("getSocket(): socket not open, unsetting and trying again...");
         socketPromise = null;
         return getSocket();
       }
     }
 
     if (!socketPromise) {
-      console.debug("getSocket(): no socket, opening one");
+      debug("getSocket(): no socket, opening one");
       socketPromise = retry(open);
       const currentSocket = socketPromise;
-      console.debug("getSocket(): awaiting socket");
+      debug("getSocket(): awaiting socket");
       const socket = await currentSocket;
-      console.debug("getSocket(): binding listeners");
+      debug("getSocket(): binding listeners");
       socket.addEventListener("error", (event) => {
-        console.debug("socket error, closing and getting new one...");
+        debug("socket error, closing and getting new one...");
         // try {
         //   socket.close();
         // } catch {}
@@ -89,7 +89,7 @@ export function createSocket({
         }
       });
       socket.addEventListener("close", (event) => {
-        console.debug("socket close, getting new one...");
+        debug("socket close, getting new one...");
         if (currentSocket === socketPromise) {
           getSocket();
         }
@@ -125,26 +125,26 @@ export function createSocket({
 }
 
 async function waitForOpen(socket: WebSocket): Promise<WebSocket> {
-  console.debug("waitForOpen(): called");
+  debug("waitForOpen(): called");
   switch (socket.readyState) {
     case socket.OPEN:
-      console.debug("waitForOpen(): socket is open, returning");
+      debug("waitForOpen(): socket is open, returning");
       return socket;
     case socket.CLOSING:
-      console.debug("waitForOpen(): socket is closing, throwing");
+      debug("waitForOpen(): socket is closing, throwing");
       throw new Error("WebSocket closing.");
     case socket.CLOSED:
-      console.debug("waitForOpen(): socket is closed, throwing");
+      debug("waitForOpen(): socket is closed, throwing");
       throw new Error("WebSocket closed.");
     case socket.CONNECTING:
-      console.debug("waitForOpen(): socket is connecting, adding listener");
+      debug("waitForOpen(): socket is connecting, adding listener");
       await new Promise((resolve, reject) => {
         socket.addEventListener("open", resolve, { once: true });
         // TODO: reject with proper errors
         socket.addEventListener("close", reject, { once: true });
         socket.addEventListener("error", reject, { once: true });
       });
-      console.debug("waitForOpen(): open, returning");
+      debug("waitForOpen(): open, returning");
       return socket;
     default:
       throw new Error(`Unexpected WebSocket readyState: ${socket.readyState}`);
